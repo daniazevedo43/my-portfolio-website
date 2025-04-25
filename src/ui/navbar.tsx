bb428784aa716bf4c7d7dcfb7e1b80d681884a2d
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-04-25 19:09:37 +0100

    Tailwind fix

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -15,8 +15,13 @@ export default function Navbar() {
     <div>
         <nav className="flex items-center justify-between flex-wrap bg-[#1976d2] p-4">
             <Link href="/" className="flex items-center flex-shrink-0 text-white mr-6">
-                <span className={`${kaushan.className} text-2xl`}>Daniel Azevedo</span>
+                <span className={`${kaushan.className} text-xl`}>Daniel Azevedo</span>
             </Link>
+            <div className="block lg:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
             <div>
                 <Link 
                     href="/" 
