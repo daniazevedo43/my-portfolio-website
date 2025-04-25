62a1b359376383bd4c1585605a8bb47dda293e46
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-04-25 16:49:13 +0100

    Navbar created

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <div className='flex justify-center items-center space-x-20 mt-30'>
+      <div className='flex justify-center items-center space-x-20 mt-20'>
         <div>
           <Image 
             className='rounded-full'
