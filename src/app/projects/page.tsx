1863be6839050a4da8c56516afe30e3a684ff415
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-02 17:54:00 +0100

    Cards content aligned

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div style={{border: '1px solid red'}}>
-        <div style={{border: '1px solid red'}} className='flex justify-center mt-12'>
+    <div>
+        <div className='flex justify-center mt-12'>
             <h2 className='text-4xl font-semibold font-ubuntu'>My Projects</h2>
         </div>
         <div className='flex justify-center'>
           <div 
-            className='flex flex-col justify-start mt-12 w-90 bg-[#FFFFFF]'
+            className='flex flex-col justify-start bg-[#FFFFFF] mt-12 w-80 shadow-lg'
           >
-              <div className='m-4 border border-gray-200'>
+              <div className='m-3 border border-gray-200'>
                 <img src='/tap-my-back.png' />
               </div>
-              <div className='m-4'>
+              <div className='m-3'>
                 <h2 className='text-2xl font-semibold font-ubuntu'>Tap My Back</h2>
                 <p className='font-ubuntu mt-3'>Posted: </p>
                 <p className='font-ubuntu mt-7'>Read more</p>
