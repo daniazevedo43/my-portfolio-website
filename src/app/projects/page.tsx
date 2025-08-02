4a0030e1f2ebb0b8bd4a290121c6f4d1e5f5ddc7
Daniel Azevedo <dani_ts_azevedo@hotmail.com>
2025-08-02 16:37:54 +0100

    Projects page created

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div className='flex justify-center mt-12'>
+        <h2 className='text-4xl font-semibold font-ubuntu'>My Projects</h2>
+    </div>
+  )
+}
